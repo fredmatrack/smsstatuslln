5dc1562fa0728c6b31fd837c0df1897238140927
fredmatrack <geert.huylebroeck@piustien.net>
2017-01-13 13:58:40 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,20 @@
 
 <?php
 	
-	$platform = 'https://MIJNSCHOOL.smartschool.be/Webservices/V3?wsdl';
-	$paswoord = 'PASSWORDWEBSERVICES';
+	$platform = 'https://UWPLATFORM.smartschool.be/Webservices/V3?wsdl';
+	$paswoord = 'paswoord_webservice_smartschool';
+	
+	
+	
+	
+	
+	
 	
 	$client = new SoapClient($platform);
 	$tmp_arr_klassen = $client->getClassList($paswoord);
 	
 	$tmp_arr_klassen = unserialize($tmp_arr_klassen);
 	
-	/*
-	echo "<pre>";
-	var_dump($tmp_arr_klassen);
-	echo "</pre>";
-	exit;
-	
-	*/
 	
 	$arr_klassen = array();
 	
@@ -82,7 +81,7 @@
             			
 	            			$klas = $_GET['lstKlas'];
 	            			
-			                $client = new SoapClient($platform);
+			                $client 	  = new SoapClient($platform);
 			                $wsresult = $client->getAllAccounts($paswoord,"$klas","1");
 		                
 		                	$xmlresult = base64_decode($wsresult);
@@ -119,60 +118,54 @@
 	                	$voornaam     = isset($_GET['voornaam'])?$_GET['voornaam']:'';
 	                
 	                	// SOAP request naar Smartschool en alle leerlingen opvragen
-	                		$wsresult = $client->getAllAccountsExtended($paswoord, $klas,"0");
+	                		$wsresult = $client->getUserDetailsByNumber($paswoord, $internnummer);
 	                	
-	                	// wsresult verwerken json
-	                		$arr_lln = json_decode($wsresult);
-	                		
-	                		$gevonden = false;
-	                		if(is_array($arr_lln)){
-	                	    	foreach ($arr_lln as $lln) {
-	                	    		if($lln->internnummer == $internnummer){
-	                	    			$gevonden = true;
-	                	    			
-	                	    			echo "<div>";
-	                	    			echo "$naam $voornaam";
-	                	    			echo "</div>";
-	                	    			
-	                	    			
-	                	    			echo "<div>";
-	                	        		echo "Huidige Smartschool-status: $lln->status";
-	                	    			echo "</div>";
-	                	    			
-	                	    			
-	                	    			
-	                	    			echo "<br><br>";
-	                	    			
-	                	    			echo "<div>";
-	                	    				if(trim($lln->status) == 'uitgeschakeld' || trim($lln->status) =='inactief' || trim($lln->status) =='inactive' || trim($lln->status) =='disabled' || trim($lln->status) =='administratief'){
-	                	    					echo "<a href=\"index.php?actie=activeerleerling&lstKlas=$klas&internnummer=$internnummer\">Account terug activeren</a>";
-	                	    				}
-	                	    			echo "</div>";
-	                	    			
-	                	    			
-	                	    			echo "<div style=\"font-weight: bold;\">";
-	                	    			echo "Paswoord resetten";
-	                	    			echo "</div>";
-	                	    			
-	                	    			echo "<div>Nieuw paswoord:</div>";
-	                	    			echo "<form action=\"index.php\" method=\"GET\">";
-	                	    			
-	                	    				echo "<input type=\"hidden\" name=\"actie\" value=\"paswoordinstellen\">";
-	                	    				echo "<input type=\"hidden\" name=\"internnummer\" value=\"$internnummer\">";
-	                	    				echo "<input type=\"hidden\" name=\"lstKlas\" value=\"$klas\">";
-	                	    				
-		                	    			echo "<div><input type=\"text\" name=\"txtPaswoord\" size=\"15\"></div>";
-		                	    			echo "<div><input type=\"submit\" value=\"Wijzig paswoord\"></div>";
-	                	    			echo "</form>";
-	                	    			
-	                	    		}
-	                	    	}
+	                		if(is_int($wsresult)){
+	                			echo "<div>Leerling niet gevonden in Smartschool!</div>";
 	                		}
 	                		
-	                		if($gevonden == false){
-	                			echo "Leerling niet gevonden in Smartschool!";
+	                		else {
+	                			// wsresult verwerken json
+	                			$lln = json_decode($wsresult);
+	                			
+	                			echo "<div style=\"font-weight: bold;\">";
+	                			echo "$naam $voornaam";
+	                			echo "</div>";
+	                			
+	                			
+	                			echo "<div>";
+	                			echo "Huidige Smartschool-status: $lln->status";
+	                			echo "</div>";
+	                			
+	                			echo "<div>&nbsp;</div>";
+	                			
+	                			echo "<div>";
+	                				if(trim($lln->status) == 'uitgeschakeld' || trim($lln->status) =='inactief' || trim($lln->status) =='inactive' || trim($lln->status) =='disabled' || trim($lln->status) =='administratief'){
+	                					echo "<a href=\"index.php?actie=activeerleerling&lstKlas=$klas&internnummer=$internnummer\">Account terug activeren</a>";
+	                				}
+	                			echo "</div>";
+	                			
+	                			echo "<div>&nbsp;</div>";
+	                			echo "<div>&nbsp;</div>";
+	                			
+	                			echo "<div style=\"font-weight: bold;\">";
+	                			echo "Paswoord resetten";
+	                			echo "</div>";
+	                			
+	                			echo "<div>Nieuw paswoord:</div>";
+	                			echo "<form action=\"index.php\" method=\"GET\">";
+	                			
+	                				echo "<input type=\"hidden\" name=\"actie\" value=\"paswoordinstellen\">";
+	                				echo "<input type=\"hidden\" name=\"internnummer\" value=\"$internnummer\">";
+	                				echo "<input type=\"hidden\" name=\"lstKlas\" value=\"$klas\">";
+	                				
+	                				echo "<div><input type=\"text\" name=\"txtPaswoord\" size=\"15\"></div>";
+	                				echo "<div><input type=\"submit\" value=\"Wijzig paswoord\"></div>";
+	                			echo "</form>";
 	                		}
-	                
+	                		
+	                		
+               
 	                }
 	                
 	                if(isset($_GET['actie']) &&  $_GET['actie'] == 'activeerleerling' && isset($_GET['internnummer']) && isset($_GET['lstKlas']) && in_array($_GET['lstKlas'], $arr_klassen)){
